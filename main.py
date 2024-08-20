d63d6df470fb63f5600c607e97ca710209eba545
ant1w3 <me@ant1w3.dev>
2024-08-20 21:22:57 +0200

    Working bookbot solution

--- main.py
+++ main.py
@@ -1,20 +1,51 @@
 def main():
   book_path = "books/frankenstein.txt"
   text = get_book_text(book_path)
-  # print(text)
   count = word_count(text)
-  print(count)
+  char_frequencies = count_char_frequencies(text)
+  
+  print_report(book_path, count, char_frequencies)
 
   
 def get_book_text(path):
-  with open("books/frankenstein.txt") as file:
+  with open(path) as file:
     text = file.read()
   return text
+
 
 def word_count(text):
   words = text.split()
   return len(words)
-    
+
+
+def count_char_frequencies(s):
+  d = {}
   
+  for c in s:
+    lowered_c = c.lower()
+    
+    if lowered_c not in d:
+      d[lowered_c] = 1
+    else:
+      d[lowered_c] += 1
+  
+  return d
+  
+
+def print_report(path, count, dict):
+
+  s = sorted(dict.items(), key= lambda item: item[1], reverse=True)
+  
+  print(f"--- Begin report of {path} ---")
+  print(f"{count} words found in the document")
+  print()
+  
+  for item in s:
+    if item[0].isalpha():
+      print(f"The '{item[0]}' character was found {item[1]} times")
+  
+  print(f"--- End report ---")
+  
+      
 main()
 
